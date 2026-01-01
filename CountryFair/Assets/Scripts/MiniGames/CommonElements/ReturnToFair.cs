a502a7b2d9dfb3f8b4f4cf27f8dd25bbada7c93b
davidpalricas <davipalricas22@gmail.com>
2026-01-01 19:42:04 +0000

    Return to Fair in Frisbee Game

---
+++ CountryFair/Assets/Scripts/MiniGames/CommonElements/ReturnToFair.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ReturnToFair : MonoBehaviour
+{
+    public void Return()
+    {
+        SceneManager.LoadScene("CountryFair");
+    }
+}
