9eb3be7172d958d4be94bd912e503b60d94c5bff
davidpalricas <davipalricas22@gmail.com>
2026-01-11 16:21:40 +0000

    Fix bug of the corwd in Archery Game Jumping while airborne

--- CountryFair/Assets/Scripts/MiniGames/ArcheryGame/Persons/Crowd.cs
+++ CountryFair/Assets/Scripts/MiniGames/ArcheryGame/Persons/Crowd.cs
@@ -6,9 +6,6 @@ public class Crowd: MonoBehaviour
 
     public UnityEvent<AudioManager.GameSoundEffects, GameObject> cheer;
 
-    private AudioManager.GameSoundEffects _cheerSoundEffect = AudioManager.GameSoundEffects.CHEER_SOUND_EFFECT;
-
-
     private void Awake()
     {
         people = GetComponentsInChildren<IdlePerson>();
