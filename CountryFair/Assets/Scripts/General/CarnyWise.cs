c8e2d19b8ea7b60d0a785b526e852629a3a767fd
davidpalricas <davipalricas22@gmail.com>
2026-01-14 15:54:56 +0000

    Adaptation System Initial Structure

---
+++ CountryFair/Assets/Scripts/General/CarnyWise.cs
@@ -0,0 +1,127 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CannyWise : MonoBehaviour
+{
+    [Header("Adaptation Configuration")] 
+    [SerializeField, Range(0f, 1f)]
+    private float precisionThresholdToIncreaseDiff = 0.7f; 
+
+    [SerializeField, Range(0f, 1f)]
+    private float precisionThresholdToDecreaseDiff = 0.3f; 
+
+    [SerializeField]
+    private int thresholdToChangeDiff = 3; 
+
+    // Variáveis de Estado
+    private float _taskStartTime = 0f;
+    private int _currentAttemptsForTask = 0; 
+    
+    // Contadores de buffer
+    private int _struggleCounter = 0;
+    private int _excelCounter = 0;
+
+    private readonly string _sessionDate = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+
+    private readonly List<float> _tasksTimes = new ();
+    private readonly List<float> _tasksPrecisions = new ();
+
+    public void StartTaskTimer()
+    {   // If the timer already started, do not reset
+        if (_taskStartTime != 0f)
+        {
+             _taskStartTime = Time.time;
+            _currentAttemptsForTask = 0; 
+        }
+    }
+
+    public void PlayerMissed()
+    {
+        _currentAttemptsForTask++;
+    }
+
+    public void PlayerScored()
+    {
+        _currentAttemptsForTask++;
+
+        float timeTaken = Time.time - _taskStartTime;
+
+        const int SCORE_ATTEMPT = 1; 
+
+        float taskPrecision = SCORE_ATTEMPT / (float)_currentAttemptsForTask; 
+
+        _tasksPrecisions.Add(taskPrecision);
+        _tasksTimes.Add(timeTaken);
+
+        EvaluatePerformance(taskPrecision, timeTaken);
+    }
+
+    private void EvaluatePerformance(float precision, float timeTaken)
+    {
+        if (precision >= precisionThresholdToIncreaseDiff)
+        {
+            _excelCounter++;
+            _struggleCounter = 0; 
+            
+            CheckToChangeDifficulty();
+            return;
+        }
+
+        if (precision <= precisionThresholdToDecreaseDiff)
+        {
+            _struggleCounter++;
+            _excelCounter = 0; 
+
+            CheckToChangeDifficulty();
+
+            return;
+        }
+
+        MantainFlowState();
+    }
+
+
+    private void MantainFlowState()
+    {
+        if (_struggleCounter > 0)
+        {
+            _struggleCounter--;
+        }
+
+        if (_excelCounter > 0)
+        {
+            _excelCounter--;
+        }
+    }
+
+    private void CheckToChangeDifficulty()
+    {
+        if (_excelCounter >= thresholdToChangeDiff)
+        {
+            Debug.Log(">>> AUMENTAR DIFICULDADE DO JOGO <<<");
+
+            _excelCounter = 0; // Reset após mudança
+
+            return;
+        }
+
+        if (_struggleCounter >= thresholdToChangeDiff)
+        {
+            Debug.Log(">>> DIMINUIR DIFICULDADE DO JOGO <<<");
+
+            _struggleCounter = 0; // Reset após mudança
+        }
+    }
+
+    public void SessionGoalReached()
+    {
+        SaveData();
+    }
+
+    private void SaveData()
+    {
+       float averageTaskPrecision = _tasksPrecisions.Average();
+       float averageTaskTime = _tasksTimes.Average();
+    }
+}
